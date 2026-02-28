f64a3b1aa52918eb1ff56a75d11eec2c1f0eca49
Jeremy Burns <jburns3222@students.cptc.edu>
2026-02-28 15:55:42 -0800

    Issue#16 Edit product (#28)
    * finishing touches
    
    * Added back changes that i know work that didnt get fully committed
    
    * Add Entity Framework Core Packages to project
    
    * Add ProductDbContext for managing Product entities
    
    * Add connection string for local SQL Server database
    
    * Add DbContext configuration for ProductDbContext
    
    * Change Price type to decimal and update migrations
    
    * Revert "Create README.md"
    
    This reverts commit bebc6b85aeae5d9e30315fc38f0324075426b297.
    
    * Add ProductController and Create product view
    
    * Revert "Add ProductController and Create product view"
    
    This reverts commit df3042de2bc003c3ec3362f7620c36692c7ac746.
    
    * Undid the Issue#14-CreateProduct branch that i committed, this is just code i did before and know works
    
    * Add "Product Listings" navigation item
    
    * Reapply "Add ProductController and Create product view"
    
    This reverts commit 662b9f4edf77a997eef38291a6b579d5c72b634b.
    
    * Revert "Undid the Issue#14-CreateProduct branch that i committed, this is just code i did before and know works"
    
    This reverts commit ff62f9110374f31e87c7bc41fe149125f80dc93a.
    
    * Add "Product Listings" navigation item
    
    * Enhance product list functionality
    
    * Add success message for product creation
    
    * Added an Update product link on the product listing page
    
    * Add Edit action to ProductController
    
    * Add product editing form and update project dependencies
    
    * Add Edit action to ProductController
    
    * Create README.md with project overview
    
    Added initial README with project details and features.

--- eCommerce/Controllers/ProductController.cs
+++ eCommerce/Controllers/ProductController.cs
@@ -42,4 +42,34 @@ public class ProductController : Controller
 		}
 		return View(p); // If model state is invslid, return the view with the product data and validation errors
 	}
+
+	[HttpGet]
+	public IActionResult Edit(int id)
+	{
+		Product? product = _context.Products
+			.Where(p => p.ProductId == id)
+			.FirstOrDefault();
+
+		if (product == null)
+		{
+			return NotFound();
+		}
+
+		return View(product);
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> Edit(Product product)
+	{
+		if (ModelState.IsValid)
+		{
+			_context.Update(product); // Update the product in the context
+			await _context.SaveChangesAsync(); // Save changes to the database
+
+			TempData["Message"] = $"{product.Title} was updated successfully!";
+			return RedirectToAction(nameof(Index));
+		}
+
+		return View(product);
+	}
 }
