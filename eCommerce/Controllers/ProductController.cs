ded9bbf071779ea38df0a0fee61eedcb45448763
Jeremy-Burns0212 <208514124+Jeremy-Burns0212@users.noreply.github.com>
2026-02-28 15:20:43 -0800

    Add Edit action to ProductController

--- eCommerce/Controllers/ProductController.cs
+++ eCommerce/Controllers/ProductController.cs
@@ -42,4 +42,19 @@ public class ProductController : Controller
 		}
 		return View(p); // If model state is invslid, return the view with the product data and validation errors
 	}
+
+	[HttpGet]
+	public IActionResult Edit(int id)
+	{
+		Product? product = _context.Products
+			.Where(p => p.ProductId == id)
+			.FirstOrDefault();
+
+		if (product == null)
+		{
+			return NotFound();
+		}
+
+		return View(product);
+	}
 }
