20020377241e95dffa7298fea52979bddd91295c
Jeremy-Burns0212 <208514124+Jeremy-Burns0212@users.noreply.github.com>
2026-02-28 17:18:24 -0800

    Add delete functionality for products

--- eCommerce/Controllers/ProductController.cs
+++ eCommerce/Controllers/ProductController.cs
@@ -42,4 +42,17 @@ public class ProductController : Controller
 		}
 		return View(p); // If model state is invslid, return the view with the product data and validation errors
 	}
+
+	public IActionResult Delete(int id)
+	{
+		Product? product = _context.Products
+			.Where(p => p.ProductId == id).FirstOrDefault();
+
+		if (product == null)
+		{
+			return NotFound();
+		}
+
+		return View(product);
+	}
 }
